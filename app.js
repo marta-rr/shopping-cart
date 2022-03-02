89859cde0aeea2742e87230f531d7ed125f70c45
MARTA <martariosrguez@hotmail.com>
2022-03-02 17:38:56 +0100

    totalprice + input item

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("working")
+ $('tbody tr').each(function (i, ele) {
+    console.log($(ele).children().first().text());
+  });
+
+  let updatePricePerItem = function (ele) {
+    let quantity = parseFloat($(ele).children('.amount').text());
+    let pricePerUnit = parseFloat($(ele).children('.pricePerUnit').text().replace(/\$/,""));
+    let pricePerItem = quantity * pricePerUnit;
+    $(ele).children('.pricePerItem').html('$' + pricePerItem);
+    return pricePerItem;
+}
+
+
+
+let updateTotalPrice = function () {
+  let prices = [];
+
+  $('tbody tr').each(function (i, ele) {
+    let pricePerItem = updatePricePerItem(ele);
+    prices.push(pricePerItem);
+  });
+  console.log(prices)
+  let totalPrice = 0
+  prices.forEach( price => totalPrice += price)
+  $('#total-price').html(' ' + '$' + totalPrice);
+  console.log(totalPrice);
+}
+  $(document).ready(function () {
+  updateTotalPrice();
+    $('.btn.remove').on('click', function (event) {
+    $(this).closest('tr').remove();
+    updateTotalPrice();
+  });
+});
+
+
+
