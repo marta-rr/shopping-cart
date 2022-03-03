a29c8cc374a8f2a098100654d2c4cc4e254a04af
MARTA <martariosrguez@hotmail.com>
2022-03-03 15:10:56 +0100

    Shopping Cart

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
- $('tbody tr').each(function (i, ele) {
-    console.log($(ele).children().first().text());
-  });
-
-  let updatePricePerItem = function (ele) {
+let updatePricePerItem = function (ele) {
     let quantity = parseFloat($(ele).find('.amount input').val());
     let pricePerUnit = parseFloat($(ele).find('.pricePerUnit input').val());
     let pricePerItem = quantity * pricePerUnit;
@@ -11,29 +7,53 @@
 }
 
 
+let updateTotalPrice = function(){
+    let prices = [];
 
-let updateTotalPrice = function () {
-  let prices = [];
-
-  $('tbody tr').each(function (i, ele) {
-    let pricePerItem = updatePricePerItem(ele);
-    prices.push(pricePerItem);
-  });
-  console.log(prices)
-  let totalPrice = 0
-  prices.forEach( price => totalPrice += price)
-  $('#total-price').html(' ' + '$' + totalPrice);
-  console.log(totalPrice);
+    $('tbody tr').each(function (i, ele) {
+        let pricePerItem = updatePricePerItem(ele);
+        prices.push(pricePerItem);
+    });
+    console.log(prices)
+    let totalPrice = 0
+    prices.forEach( price => totalPrice += price)
+    $('#total-price').html(' ' + '$' + totalPrice);
+    console.log(totalPrice);
 }
+
 $(document).ready(function () {
-  updateTotalPrice();
-    $('.btn.remove').on('click', function (event) {
-    $(this).closest('tr').remove();
-    updateTotalPrice();
+        updateTotalPrice();
+        $(document).on('click', '.btn.remove', function (event) {
+        $(this).closest('tr').remove();
+        updateTotalPrice();
+        });
+        let timeout;
+        $(document).on('input', 'tr input', function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            updateTotalPrice();
+        }, 500);
 });
-$('tr input').on('input', function () {
+
+$('#addNewItem').on('submit', function (event) {
+    event.preventDefault();
+    let name = $(this).children('[name=name]').val();
+    let price = $(this).children('[name=price]').val();
+    let quantity = $(this).children('[name=quantity]').val();
+
+    $('tbody').append('<tr>' +
+        '<td class="items">' + name + '</td>' +
+        '<td class="pricePerUnit"><input type="number" value="' + price + '" /></td>' +
+        '<td class="amount"><input type="number" value="' + quantity + '" /></td>' +
+        '<td class="pricePerItem"></td>' +
+        '<td><button class="btn btn-light btn-lg remove">Remove</button></td>' +
+    '</tr>');
+
     updateTotalPrice();
-});
+    $(this).children('[name=name]').val('');
+    $(this).children('[name=price]').val('');
+    $(this).children('[name=quantity]').val('');
+    });
 });
 
 
